6357935f758fbb67eb7aece1cad9bf6c5f23e083
Atharva Khairnar <atharvakhairnar2006@gmail.com>
2026-06-18 11:53:26 +0530

    Update database.py

--- AI-Farm-Machinery-Recommendation-System/backend/database.py
+++ AI-Farm-Machinery-Recommendation-System/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root123@localhost:3306/khetseva"
+DATABASE_URL = "mysql+pymysql://root:PASSWORD@Local Host Address/MYSQL Database name"
 
 engine = create_engine(DATABASE_URL)
 
